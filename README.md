6ade0c8c8e056151ddbd7910f2ed509cd4cb093f
William0Friend <william.friend777@gmail.com>
2022-06-15 20:57:49 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-twyttj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-twyttj)
---
+++ index.js
@@ -0,0 +1,19 @@
+// run `node index.js` in the terminal
+const http = require('node:http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+console.log(
+  `Server Running... Powered bu - Node.js v${process.versions.node}!`
+);
